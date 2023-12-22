698ad0a793a10a612c1c32b51fdea8dc5bc47619
김용호 <yhkim@miridih.com>
2023-12-22 17:09:42 +0900

    vite-app 예제 작성

--- apps/vite-app/index.tsx
+++ apps/vite-app/index.tsx
@@ -1,17 +1,61 @@
+import "@cpdev2/design-system/dist/index.css";
 import React from "react";
 import { createRoot } from "react-dom/client";
-import "@cpdev2/design-system/dist/index.css";
-import { Button } from "@cpdev2/design-system";
+import { CommonButton, Typography } from "@cpdev2/design-system";
+
+const App = ({ theme }: { theme: "light" | "dark" }) => {
+  const [currentTheme, setCurrentTheme] = React.useState<typeof theme>(theme);
+  const [isPending, startTransition] = React.useTransition();
+
+  return (
+    <div
+      style={{
+        display: "contents",
+      }}
+      data-ds-theme={currentTheme}
+    >
+      <section
+        style={{
+          background: "var(--s_color_background_default)",
+          color: "var(--s_color_contents_default)",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "100vw",
+          height: "100vh",
+        }}
+      >
+        <div>
+          <Typography
+            as="h2"
+            font="title_XL"
+            color="contents_default"
+            marginTop="0"
+            marginBottom="60"
+          >
+            {isPending ? 'changing theme...' : "hello world"}
+          </Typography>
+          <CommonButton
+            display="inline-flex"
+            size="L"
+            buttonType="outline"
+            state={isPending ? "loading" : "default"}
+            onClick={() => {
+              startTransition(() => {
+                setCurrentTheme((prev) => (prev === "dark" ? "light" : "dark"));
+              });
+            }}
+          >
+            current theme: {currentTheme}
+          </CommonButton>
+        </div>
+      </section>
+    </div>
+  );
+};
 
 const element = document.getElementById("app");
 if (element) {
   const root = createRoot(element);
-  root.render(
-    <section>
-      <h1>hello world</h1>
-      <div>
-        <Button display="inline-flex">hello world</Button>
-      </div>
-    </section>,
-  );
+  root.render(<App theme="light" />);
 }
--- packages/design-system/src/sprinkles.css.ts
+++ packages/design-system/src/sprinkles.css.ts
@@ -1,27 +1,39 @@
 import { createSprinkles, defineProperties } from "@vanilla-extract/sprinkles";
 import { atomicLayer } from "./global.css";
+import {
+  alignItems,
+  all,
+  boxSizing,
+  cursor,
+  flexDirection,
+  flexWrap,
+  justifyContent,
+  position,
+  textAlign,
+  wordBreak,
+} from "./properties/base";
 
 const properties = defineProperties({
   "@layer": atomicLayer,
   properties: {
-    all: ["revert", "unset", "initial"],
-    alignItems: ["stretch", "center", "flex-start", "flex-end"],
+    all,
+    alignItems,
     appearance: ["none", "auto"],
-    boxSizing: ["border-box", "content-box"],
+    boxSizing,
     display: ["flex", "inline-flex", "none", "block"],
-    justifyContent: ["space-between", "center", "flex-start", "flex-end"],
-    flexDirection: ["row", "column", "row-reverse", "column-reverse"],
-    flexWrap: ["wrap", "nowrap"],
-    position: ["relative", "absolute", "sticky"],
-    textAlign: ["center", "left", "right"],
+    justifyContent,
+    flexDirection,
+    flexWrap,
+    position,
+    textAlign,
     width: ["auto", "fit-content", "100%"],
     height: ["auto", "fit-content", "100%"],
     overflow: ["hidden"],
     inset: ["0px"],
     padding: ["0px"],
     flex: ["0 0 auto", "1 1 auto", "1 1 0%", "1 1 100%"],
-    cursor: ["auto", "pointer", "not-allowed", "inherit"],
-    wordBreak: ["normal", "break-all", "keep-all", "break-word"],
+    cursor,
+    wordBreak,
   },
 });
 
